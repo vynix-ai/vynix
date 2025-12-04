277e888e788f0b4ae520fe6cd49b9ff6a1f50505
vynix <kalijunaidtest@gmail.com>
2025-12-04 14:30:37 -0400

    Refactor parse module: consolidate parsing functions and remove deprecated files
    - Introduced a new `parse.py` file consolidating various parsing utilities.
    - Removed obsolete `__init__.py`, `fuzzy_parse_json.py`, `to_dict.py`, `to_json.py`, `to_num.py`, `to_xml.py`, `xml_parser.py`, and `breakdown_pydantic_annotation.py` files.
    - Updated `framework_options.json` to enhance mathematical framework options.

--- vynix/libs/file/concat.py
+++ vynix/libs/file/concat.py
@@ -1,12 +1,7 @@
-# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
-#
-# SPDX-License-Identifier: Apache-2.0
-
 from pathlib import Path
 from typing import Any
 
-from lionagi.libs.file.create_path import create_path
-from lionagi.utils import lcall
+from lionagi.utils import create_path, lcall
 
 from .process import dir_to_files
 
