71e7da574cd2a8ab80ee2e9a14139a3a47644518
vynix <kalijunaidtest@gmail.com>
2026-01-04 09:06:51 -0400

    refactor: reorder imports for consistency and clarity

--- vynix/ln/fuzzy/__init__.py
+++ vynix/ln/fuzzy/__init__.py
@@ -1,7 +1,7 @@
 from ._extract_json import extract_json
 from ._fuzzy_json import fuzzy_json
 from ._fuzzy_match import FuzzyMatchKeysParams, fuzzy_match_keys
-from ._fuzzy_validate import fuzzy_validate_pydantic, fuzzy_validate_mapping
+from ._fuzzy_validate import fuzzy_validate_mapping, fuzzy_validate_pydantic
 from ._string_similarity import SIMILARITY_TYPE, string_similarity
 from ._to_dict import to_dict
 
